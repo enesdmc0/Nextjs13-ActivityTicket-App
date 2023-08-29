8880edf0b79c6add1001bfae6e85e9a46b695fd9
enesdmc0 <enesdmcc@gmail.com>
2023-08-29 23:15:17 +0300

    Detail page swiper completed

--- app/(root)/(routes)/[activityId]/(routes)/page.tsx
+++ app/(root)/(routes)/[activityId]/(routes)/page.tsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import SwiperDetail from "@/components/SwiperDetail";
+import {redirect} from "next/navigation";
+import prisma from "@/lib/prismadb";
+import {Separator} from "@/components/ui/separator";
+import Map from "@/components/Map";
 
-const ActivityDetail = () => {
+const ActivityDetail = async ({params}: { params: { activityId: string } }) => {
+
+
+    if (!params.activityId) {
+        redirect("/")
+    }
+
+    const activity = await prisma?.activity.findUnique({
+        where: {
+            id: params.activityId
+        }
+    })
+
+    if (!activity) {
+        redirect("/")
+    }
+
     return (
-        <div>
-          dwdw
+        <div className="mt-10 space-y-10 w-3/4 mx-auto">
+            <div className="space-y-5">
+                <h2 className="text-xl font-bold">{activity.title}</h2>
+                <SwiperDetail activity={activity}/>
+            </div>
+            <Separator/>
+            <Map/>
+            <Separator/>
         </div>
     );
 };
---
+++ components/SwiperDetail/index.tsx
@@ -0,0 +1,42 @@
+"use client"
+import React from 'react';
+import {EffectFade, Autoplay} from 'swiper/modules';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import { Activity } from '@prisma/client';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import Image from "next/image"
+
+interface Props  {
+    activity: Activity
+}
+
+const SwiperDetail: React.FC<Props> = ({activity}) => {
+
+    return (
+        <div>
+            <Swiper
+                spaceBetween={10}
+                modules={[Autoplay, EffectFade]}
+                effect={'fade'}
+                className="h-[400px] rounded-md shadow-lg"
+                autoplay={{
+                    delay: 2500,
+                    disableOnInteraction: false,
+                }}
+            >
+                
+                {activity.imagesURL.map(image => (
+                    <SwiperSlide className='relative' key={activity.id}>
+                    <Image fill src={image} className="object-cover" alt=""/>
+                    </SwiperSlide>
+                ))}
+            
+            </Swiper>
+
+        </div>
+    );
+};
+
+export default SwiperDetail;
