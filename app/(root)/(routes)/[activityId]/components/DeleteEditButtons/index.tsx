c8b2cb1ee5391151fff21856326915c2e1a5dfd1
enesdmc0 <enesdmcc@gmail.com>
2023-09-02 00:30:39 +0300

    Delete Modal completed

--- app/(root)/(routes)/[activityId]/components/DeleteEditButtons/index.tsx
+++ app/(root)/(routes)/[activityId]/components/DeleteEditButtons/index.tsx
@@ -6,33 +6,43 @@ import {Edit, Trash} from "lucide-react";
 import {useParams, useRouter} from "next/navigation";
 import axios from "axios";
 import toast from "react-hot-toast";
+import {AlertModal} from "@/modals/AlertModal";
 
 const DeleteEditButtons = () => {
     const params = useParams()
     const router = useRouter()
     const [loading, setLoading] = useState(false)
+    const [open, setOpen] = useState(false)
 
-    const handleClick = () => {
+    const handleDelete = async () => {
         try {
             setLoading(true)
-            axios.delete(`/api/activity/${params.activityId}`)
+            await axios.delete(`/api/activity/${params.activityId}`)
             router.refresh()
             toast.success("Activity Deleted")
             router.push("/")
-        }catch (error) {
+        } catch (error) {
             toast.error("Activity deleted error")
         }
     }
 
     return (
-        <div className="flex items-center justify-end gap-10">
-            <Link href={`/activity/${params.activityId}`} className={buttonVariants({variant: "secondary"})}>
-                <Edit className="h-5 w-5"/>
-            </Link>
-            <Button disabled={loading} onClick={handleClick}>
-                <Trash className="h-5 w-5"/>
-            </Button>
-        </div>
+        <>
+            <AlertModal
+                isOpen={open}
+                onClose={() => setOpen(false)}
+                onConfirm={handleDelete}
+                loading={loading}
+            />
+            <div className="flex items-center justify-end gap-10">
+                <Link href={`/activity/${params.activityId}`} className={buttonVariants({variant: "secondary"})}>
+                    <Edit className="h-5 w-5"/>
+                </Link>
+                <Button disabled={loading} onClick={() => setOpen(true)}>
+                    <Trash className="h-5 w-5"/>
+                </Button>
+            </div>
+        </>
     );
 };
 
